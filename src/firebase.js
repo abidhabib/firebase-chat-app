a4bcf8b5d672c5f3d150fc240af7aad1cede2ec5
Abid Habib <you@example.com>
2022-12-02 02:47:28 +0530

    new changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyC_730ZmO8rOnJv02KEBnaydc5d9KlnwLA",
-    authDomain: "firechat-eb0f5.firebaseapp.com",
-    projectId: "firechat-eb0f5",
-    storageBucket: "firechat-eb0f5.appspot.com",
-    messagingSenderId: "855670603066",
-    appId: "1:855670603066:web:a9bcf2477bcbd1045da403",
-    measurementId: "G-GEJDXM80CV"
+    apiKey: "AIzaSyDsc89picK5Xn68cz8zDpHh6zM5ilz72hk",
+    authDomain: "chat-ac540.firebaseapp.com",
+    projectId: "chat-ac540",
+    storageBucket: "chat-ac540.appspot.com",
+    messagingSenderId: "956207308668",
+    appId: "1:956207308668:web:47900814391616e8f50ef9",
+    measurementId: "G-DX1M283NLD"
 })
 
 const db = firebaseApp.firestore()
